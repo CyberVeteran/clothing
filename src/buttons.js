90ef10ec89622abd451ec978a05bb65602077e92
Vasyl Kliushta <castelkwm@gmail.com>
2022-08-02 22:02:09 +0300

    added onclic

--- src/buttons.js
+++ src/buttons.js
@@ -1,14 +1,18 @@
 const Buttons = () => {
+    const handleClick = ()=>{
+        console.log('it works')
+    }
     return ( 
         <div className="buttons">
-            <button>shoes</button>
             <h2>chose your shoes</h2>
-            <button>pants</button>
+            <button onClick={handleClick}>shoes</button>
             <h2>chose your pants</h2>
-            <button>shirt</button>
+            <button onClick={handleClick}>pants</button>
             <h2>chose your shirts</h2>
-            <button>hat</button>
+            <button onClick={handleClick}>shirt</button>
             <h2>chose your hats</h2>
+            <button onClick={handleClick}>hat</button>
+            
         </div>
      );
 }
